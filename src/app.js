b84507cd5a883d48f6b01f0f6bc09f462df5382c
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-04-16 11:03:24 +0000

    mi tarea

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
+
   //write your code here
+  window.onload = function() {
+    newCard()
+  }
+
   function newCard() {
     let suits = ["♦", "♥", "♠", "♣"];
     let numbers = [
@@ -23,9 +27,29 @@ window.onload = function() {
     ];
     let indexSuits = Math.floor(Math.random() * suits.length);
     let indexNumbers = Math.floor(Math.random() * numbers.length);
+    document.querySelector("#number").innerHTML = numbers[indexNumbers];
+    document.querySelector("#top").innerHTML = suits[indexSuits];
+    document.querySelector("#end").innerHTML = suits[indexSuits];
 
-    document.getElementById("number").innerHTML = "9";
-  }
+    // let colorSuits = suits[indexSuits] === '♦' || suits[indexSuits] === '♥' ? "text-danger" : ""
+    
+    let colorSuits; 
 
-  console.log("Hola");
+    if (suits[indexSuits] === '♦' || suits[indexSuits] === '♥') {
+      colorSuits = 'text-danger';
+    } else {
+      colorSuits = ''
+    };
+    
+    document.querySelector("#top").className = colorSuits;
+    document.querySelector("#end").className = colorSuits;
 };
+  
+  
+  
+
+
+  document.getElementById('btnNewCard').addEventListener("click", newCard);
+
+  setInterval(() => newCard(), 10000);
+  
