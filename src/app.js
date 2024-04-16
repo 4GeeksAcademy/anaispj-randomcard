4daf7b6bd17e895dbae8c33249c926016d3099ac
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-04-16 10:20:39 +0000

    [para edu

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function newCard() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexSuits = Math.floor(Math.random() * suits.length);
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+    document.getElementById("number").innerHTML = "9";
+  }
+
+  console.log("Hola");
 };
